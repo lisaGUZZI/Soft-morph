7fef9c8c16de8db1fd48d2f4be2262ad62aa8b6c
lisaGUZZI <85740645+lisaGUZZI@users.noreply.github.com>
2024-06-14 16:28:34 +0200

    upload code

---
+++ SoftMorph2D.py
@@ -0,0 +1,279 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class SoftErosion(nn.Module):
+    """
+    Class implemented using Pytorch module to perform differentiable soft erosion on 2D input image.
+    """
+    def __init__(self):
+        super(SoftErosion, self).__init__()
+        self.indices_list = torch.tensor([          
+            [0, 1], [0, 2], [1, 2], [2, 2],
+            [2, 1], [2, 0], [1, 0], [0, 0], [1, 1]
+        ], dtype=torch.long)
+        
+    def test_format(self, img, connectivity):
+        """
+        Function to check user inputs :
+        - Input image shape must either be [batch_size, channels, height, width] or [height, width]. 
+        - Input image values must be between 0 and 1.
+        - Connectivity represents the sutructuring element of the operation. In 2D, it must be either 4 or 8
+        """
+        dim = img.dim()
+        size = img.size()
+        if dim > 4 or dim <2:
+            raise Exception(f"Invalid input shape {size}. Expected [batch_size, channels, height, width] or [height, width]. Consider using the 3D version for 3D input images")
+        elif dim < 4:
+            if dim ==3 :
+                # If the input dimension is 3 it might be due to input format [channels, height width]
+                if size[0] > 3 : # If this is not likely we raise an exception.
+                    raise Exception(f"Ambiguous input shape {size}. Expected [batch_size, channels, height, width] or [height, width].")
+            for i in range(4-dim):img = img.unsqueeze(0) 
+            print("Image resized to : ", img.size())
+        if img.min() < 0.0 or img.max() > 1.0:
+            raise ValueError("Input image values must be in the range [0, 1].")
+        if connectivity != 4 and connectivity !=8:
+            raise ValueError("Connectivity should either be 4 or 8")
+        return img
+
+    def allcondArithm(self, n, connectivity):
+        """
+        Apply polynomial formula based on the boolean expression that defines an erosion on each 3x3 overlapping squares of the 2D image.
+        Inputs : vector of 3x3 overlapping squares n, connectivity (4 or 8) defining the structuring element.
+        Output : In binary case returns 0 if the central pixel needs to be changed to 0, returns 1 otherwise.
+        """
+        if connectivity == 4 :   
+            F = torch.prod(n[:, :, :, ::2], dim=-1)
+        else :
+            F = torch.prod(n, dim=-1)
+        return F
+    
+
+    def forward(self, im, iterations=1, connectivity = 4):
+        """
+        Inputs :
+        - im : input 2D image of shape [batch_size, channels, height, width] or [height, width].
+        - iterations : number of times the morphological operation is repeated.
+        - connectivity : connectivity representing the structuring element. Should either be 4 or 8.
+        Output : Image after morphological operation
+        """
+        im = self.test_format(im, connectivity) # Check user inputs
+        for _ in range(iterations):
+            im_padded = F.pad(im, (1, 1, 1, 1), mode='constant', value=1)
+            # Unfold the tensor to extract overlapping 3x3 windows
+            unf = nn.Unfold((im.shape[2],im.shape[3]), 1, 0, 1)
+            unfolded = unf(im_padded) 
+            unfolded = unfolded.view(im.shape[0], im.shape[1], -1, unfolded.size(-1))
+            # Apply the morphological operation formula to all windows simultaneously
+            unfolded = unfolded[:, :, :, (self.indices_list[:, 0] * 3) + self.indices_list[:, 1]]
+            output = self.allcondArithm(unfolded, connectivity)
+            # Adjust the dimensions of output to match the spatial dimensions of im
+            output = output.view(output.size(0), output.size(1), im.shape[2], im.shape[3])
+            # Element-wise multiplication
+            im = im * output
+        return im
+
+
+class SoftDilation(nn.Module):
+    """
+    Class implemented using Pytorch module to perform differentiable soft dilation on 2D input image.
+    """
+    def __init__(self):
+        super(SoftDilation, self).__init__()
+        self.indices_list = torch.tensor([
+            [0, 1], [0, 2], [1, 2], [2, 2],
+            [2, 1], [2, 0], [1, 0], [0, 0], [1,1]
+        ], dtype=torch.long)
+
+    def test_format(self, img, connectivity):
+        """
+        Function to check user inputs :
+        - Input image shape must either be [batch_size, channels, height, width] or [height, width]. 
+        - Input image values must be between 0 and 1.
+        - Connectivity represents the sutructuring element of the operation. In 2D, it must be either 4 or 8
+        """
+        dim = img.dim()
+        size = img.size()
+        if dim > 4 or dim <2:
+            raise Exception(f"Invalid input shape {size}. Expected [batch_size, channels, height, width] or [height, width]. Consider using the 3D version for 3D input images")
+        elif dim < 4:
+            if dim ==3 :
+                # If the input dimension is 3 it might be due to input format [channels, height width]
+                if size[0] > 3 : # If this is not likely we raise an exception.
+                    raise Exception(f"Ambiguous input shape {size}. Expected [batch_size, channels, height, width] or [height, width].")
+            for i in range(4-dim):img = img.unsqueeze(0) 
+            print("Image resized to : ", img.size())
+        if img.min() < 0.0 or img.max() > 1.0:
+            raise ValueError("Input image values must be in the range [0, 1].")
+        if connectivity != 4 and connectivity !=8:
+            raise ValueError("Connectivity should either be 4 or 8")
+        return img
+    
+    def allcondArithm(self, n, connec):
+        """
+        Apply polynomial formula based on the boolean expression that defines a dilation on each 3x3 overlapping squares of the 2D image.
+        Inputs : vector of 3x3 overlapping squares n, connectivity (4 or 8) defining the structuring element.
+        Output : Returns the new value attributed to each central pixel
+        """
+        if connec == 4 :
+            F = 1 - torch.prod(1 - n[:, :, :, ::2], dim=-1)
+        else :
+            F = 1 - torch.prod(1 - n, dim=-1)
+        return F
+
+    def forward(self, im, iterations=1, connectivity = 4):
+        """
+        Inputs :
+        - im : input 2D image of shape [batch_size, channels, height, width] or [height, width].
+        - iterations : number of times the morphological operation is repeated.
+        - connectivity : connectivity representing the structuring element. Should either be 4 or 8.
+        Output : Image after morphological operation
+        """
+        im = self.test_format(im, connectivity)
+        for _ in range(iterations):
+            # Unfold the tensor to extract overlapping 3x3 windows
+            unf = nn.Unfold((im.shape[2],im.shape[3]), 1, 1, 1)
+            unfolded = unf(im) 
+            unfolded = unfolded.view(im.shape[0], im.shape[1], -1, unfolded.size(-1))
+            # Apply the formula to all windows simultaneously
+            unfolded = unfolded[:, :, :, (self.indices_list[:, 0] * 3) + self.indices_list[:, 1]]
+            output = self.allcondArithm(unfolded, connectivity)
+            # Adjust the dimensions of output to match the spatial dimensions of im
+            im = output.view(output.size(0), output.size(1), im.shape[2], im.shape[3])
+        return im
+
+
+class SoftClosing(nn.Module):
+    """
+    Class implemented using Pytorch module to perform differentiable soft closing on 2D input image.
+    """
+    def __init__(self):
+        super(SoftClosing, self).__init__()
+        self.dilate = SoftDilation()
+        self.erode = SoftErosion()
+
+    def forward(self,input_img, iterations, dilation_connectivity = 4, erosion_connectivity = 4):
+        """
+        Inputs :
+        - im : input 2D image of shape [batch_size, channels, height, width] or [height, width].
+        - iterations : number of times each morphological operation is repeated.
+        - connectivity : connectivity representing the structuring element. Should either be 4 or 8.
+                         Can define different connectivity values for erosion and dilation
+        Output : Image after morphological operation
+        """
+        output = self.dilate(input_img, iterations, dilation_connectivity)
+        output = self.erode(output, iterations, erosion_connectivity)
+        return output
+    
+class SoftOpening(nn.Module):
+    """
+    Class implemented using Pytorch module to perform differentiable soft opening on 2D input image.
+    """
+    def __init__(self):
+        super(SoftOpening, self).__init__()
+        self.erode = SoftErosion()
+        self.dilate = SoftDilation()
+
+    def forward(self,input_img, iterations, dilation_connectivity = 4, erosion_connectivity = 4):
+        """
+        Inputs :
+        - im : input 2D image of shape [batch_size, channels, height, width] or [height, width].
+        - iterations : number of times each morphological operation is repeated.
+        - connectivity : connectivity representing the structuring element. Should either be 4 or 8.
+                         Can define different connectivity values for erosion and dilation
+        Output : Image after morphological operation
+        """
+        output = self.erode(input_img, iterations, erosion_connectivity)
+        output = self.dilate(output, iterations, dilation_connectivity)
+        return output
+
+
+class SoftSkeletonizer(nn.Module):
+    """
+    Class implemented using Pytorch module to perform differentiable soft skeletonization on 2D input image.
+    
+    the max_iter input represents the number of times the thinning operation will be repeated. 
+    This input will be automatically determined in future versions.
+    """
+    def __init__(self, max_iter=5):
+        super(SoftSkeletonizer, self).__init__()
+        self.maxiter = max_iter
+        # Extract ordered index list in each subdirection (North, East, South, West)
+        self.indices_list = [self.extract_indices(o) for o in range(4)]
+        
+    def test_format(self, img):
+        """
+        Function to check user inputs :
+        - Input image shape must either be [batch_size, channels, height, width] or [height, width]. 
+        - Input image values must be between 0 and 1.
+        - Connectivity represents the sutructuring element of the operation. In 2D, it must be either 4 or 8
+        """
+        dim = img.dim()
+        size = img.size()
+        if dim > 4 or dim <2:
+            raise Exception(f"Invalid input shape {size}. Expected [batch_size, channels, height, width] or [height, width]. Consider using the 3D version for 3D input images")
+        elif dim < 4:
+            if dim ==3 :
+                # If the input dimension is 3 it might be due to input format [channels, height width]
+                if size[0] > 3 : # If this is not likely we raise an exception.
+                    raise Exception(f"Ambiguous input shape {size}. Expected [batch_size, channels, height, width] or [height, width].")
+            for i in range(4-dim):img = img.unsqueeze(0) 
+            print("Image resized to : ", img.size())
+        if img.min() < 0.0 or img.max() > 1.0:
+            raise ValueError("Input image values must be in the range [0, 1].")
+        return img
+    
+    def extract_indices(self, o):
+        """
+        Function to extract extract ordered index list in each subdirection (North, East, South, West)
+        """
+        indices = torch.tensor([
+            [0, 1], [0, 2], [1, 2], [2, 2],
+            [2, 1], [2, 0], [1, 0], [0, 0]
+        ], dtype=torch.long)
+        # Adjust indices based on orientation
+        indices = torch.roll(indices, -2 * o, dims=0)
+        
+        return indices
+
+    def allcondArithm(self, n):
+        """
+        Apply polynomial formula based on the boolean expression that defines a thinning operation on each 3x3 overlapping squares of the 2D image.
+        Inputs : vector of 3x3 overlapping squares n.
+        Output : In binary case returns 0 if the central pixel needs to be changed to 0, returns 1 otherwise.
+        """
+        F1 = (1 - n[:, :, :, 0])
+        F2 = ((1 - n[:, :, :, 1]) * (1 - n[:, :, :, 7]) * (1-n[:, :, :, 2]-n[:, :, :, 3]+2*n[:, :, :, 2]*n[:, :, :, 3]-n[:, :, :, 4]+2*n[:, :, :, 2]*n[:, :, :, 4]+2*n[:, :, :, 3]*n[:, :, :, 4]-4*n[:, :, :, 2]*n[:, :, :, 3]*n[:, :, :, 4]) *
+            (n[:, :, :, 3] + n[:, :, :, 5] - 2 * (n[:, :, :, 3] * n[:, :, :, 5])) * (n[:, :, :, 3] + n[:, :, :, 6] - 2 * (n[:, :, :, 3] * n[:, :, :, 6])))
+        F3 = ((n[:, :, :, 1] + n[:, :, :, 5] - 2 * (n[:, :, :, 1] * n[:, :, :, 5])) * (n[:, :, :, 2] + n[:, :, :, 5] - 2 * (n[:, :, :, 2] * n[:, :, :, 5])) *
+            (n[:, :, :, 4] + (1 - n[:, :, :, 5]) - 2 * (n[:, :, :, 4] * (1 - n[:, :, :, 5]))) * (1 - n[:, :, :, 6]) * (1 - n[:, :, :, 7]))
+        F4 = (n[:, :, :, 2] * n[:, :, :, 4] * (1 - n[:, :, :, 7]))
+        F5 = ((1 - n[:, :, :, 1]) * n[:, :, :, 4] * n[:, :, :, 6])
+        F6 = ((1 - n[:, :, :, 1]) * (1 - n[:, :, :, 2]) * (1 - n[:, :, :, 3]) * n[:, :, :, 6] * n[:, :, :, 7])
+        F = 1-(F1 * (1 - ((1 - F2) * (1 - F3) * (1 - F4) * (1 - F5) * (1 - F6))))
+        return F
+
+    def forward(self, im):
+        """
+        Input :
+        - im : input 2D image of shape [batch_size, channels, height, width] or [height, width].
+        Output : Image after morphological operation
+        """
+        im = self.test_format(im)
+        for _ in range(self.maxiter):
+            for o in range(4): # Iterate through all directions
+                # Unfold the tensor to extract overlapping 3x3 windows
+                unf = nn.Unfold((im.shape[2],im.shape[3]), 1, 1, 1)
+                unfolded = unf(im) 
+                unfolded = unfolded.view(im.shape[0], im.shape[1], -1, unfolded.size(-1))
+                # Apply the formula to all windows simultaneously
+                unfolded = unfolded[:, :, :, (self.indices_list[o][:, 0] * 3) + self.indices_list[o][:, 1]]
+                output = self.allcondArithm(unfolded)
+                # Adjust the dimensions of output to match the spatial dimensions of im
+                output = output.view(output.size(0), output.size(1), im.shape[2], im.shape[3])
+                # Element-wise multiplication
+                im = im * output
+        return im   
